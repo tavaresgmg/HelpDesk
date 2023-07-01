4d5b01515020b8c37960b99a6b9b5151dbac43b5
Guilherme Tavares <tavaresgmg@gmail.com>
2023-07-01 18:00:57 -0300

    Criação da Conexão com o banco de dados
    inserção de código e driver para fazer a conexão com o mysql

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/helpdesk";
+        String username = "tavares";
+        String password = "Tav1912@";
+
+        try {
+            // Registrar o driver JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Estabelecer a conexão com o banco de dados
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Realizar operações no banco de dados...
+            String sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, "Nome do Usuário");
+                statement.setString(2, "email@example.com");
+
+                statement.executeUpdate();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+            // Fechar a conexão
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver JDBC não encontrado!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Erro ao estabelecer conexão com o banco de dados!");
+            e.printStackTrace();
+        }
+    }
+}
