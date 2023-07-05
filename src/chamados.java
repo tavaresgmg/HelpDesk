a835c565ef01a7aa390b8718dc2927f8038e793b
Guilherme Tavares <tavaresgmg@gmail.com>
2023-07-05 01:19:21 -0300

    acréscimo do código chamados

--- src/chamados.java
+++ src/chamados.java
@@ -1,4 +1,5 @@
 package src;
+import java.util.Date;
 
 public class chamados {
     private int id;
@@ -18,4 +19,62 @@ public class chamados {
         this.dataCriacao = dataCriacao;
         this.idUsuario = idUsuario;
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getTitulo(int id){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public String getPrioridade(){
+        return prioridade;
+    } 
+
+    public void setPrioridade(String prioridade){
+        this.prioridade = prioridade;
+    }
+
+    public Date getDataCriacao(){
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao){
+        this.dataCriacao = dataCriacao;
+    }
+
+    public int getIdUsuario(){
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario){
+        this.idUsuario = idUsuario;
+    }
 }
+
+
